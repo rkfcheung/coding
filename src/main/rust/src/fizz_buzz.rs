ce2afaf99598d15c347679976fae132337795ca6
Raymond Cheung <korekontakt@gmail.com>
2024-02-24 09:33:51 +0000

    Use match instead of if/else

--- src/main/rust/src/fizz_buzz.rs
+++ src/main/rust/src/fizz_buzz.rs
@@ -22,6 +22,15 @@ impl Solution {
         
         result
     }
+
+    pub fn fizz_buzz_match(n: i32) -> Vec<String> {
+        (1..=n).map(|i| match (i % 3, i % 5) {
+            (0, 0) => "FizzBuzz".to_string(),
+            (0, _) => "Fizz".to_string(),
+            (_, 0) => "Buzz".to_string(),
+            _ => i.to_string(),
+        }).collect()
+    }
 }
 
 #[cfg(test)]
@@ -30,23 +39,29 @@ mod tests {
 
     #[test]
     fn test_value_eq_3() {
-        assert_eq!(Solution::fizz_buzz(3), *vec!["1".to_string(), "2".to_string(), "Fizz".to_string()]);
+        let expected = vec!["1".to_string(), "2".to_string(), "Fizz".to_string()];
+
+        assert_eq!(Solution::fizz_buzz(3), *expected);
+        assert_eq!(Solution::fizz_buzz_match(3), *expected);
     }
 
     #[test]
     fn test_value_eq_5() {
-        assert_eq!(Solution::fizz_buzz(5), *vec![
+        let expected = vec![
             "1".to_string(),
             "2".to_string(),
             "Fizz".to_string(),
             "4".to_string(),
             "Buzz".to_string(),
-        ]);
+        ];
+
+        assert_eq!(Solution::fizz_buzz(5), *expected);
+        assert_eq!(Solution::fizz_buzz_match(5), *expected);
     }
 
     #[test]
     fn test_value_eq_15() {
-        assert_eq!(Solution::fizz_buzz(15), *vec![
+        let expected = vec![
             "1".to_string(),
             "2".to_string(),
             "Fizz".to_string(),
@@ -62,6 +77,9 @@ mod tests {
             "13".to_string(),
             "14".to_string(),
             "FizzBuzz".to_string(),
-        ]);
+        ];
+
+        assert_eq!(Solution::fizz_buzz(15), *expected);
+        assert_eq!(Solution::fizz_buzz_match(15), *expected);
     }
 }
