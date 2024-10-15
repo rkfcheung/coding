666260016aa965918803bd16fd32a01e0193ebc5
Raymond Cheung <korekontakt@gmail.com>
2024-10-15 21:57:37 +0100

    Add for loop

--- src/main/c/ch01/fahr.c
+++ src/main/c/ch01/fahr.c
@@ -5,17 +5,25 @@
     for fahr = 0, 20, ..., 300
 */
 main() {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0; // lower limit of temperature table
     upper = 300; // upper limit
     step = 20; // step size
 
+    printf("Using while loop ...\n");
     fahr = lower;
-    while (fahr <= upper) {
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+    while (fahr <= upper / 2) {
+        celsius = 5.0 * (fahr - 32.0) / 9.0;
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
         fahr += step;
+    }
+
+    printf("\n");
+    printf("Using for loop ...\n");
+    for (; fahr <= upper; fahr += step) {
+        celsius = (5.0 / 9.0) * (fahr - 32);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
     }
 }
