4b152b13af56d7bbe25a02aeb960ad9b2d665771
Raymond Cheung <korekontakt@gmail.com>
2024-10-15 21:59:35 +0100

    Use constants

--- src/main/c/ch01/fahr.c
+++ src/main/c/ch01/fahr.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 
+#define LOWER 0 // lower limit of temperature table
+#define UPPER 300 // upper limit
+#define STEP 20 // step size
+
 /* 
     print Fahrenheit-Celsius table
     for fahr = 0, 20, ..., 300
 */
 main() {
     float fahr, celsius;
-    int lower, upper, step;
-
-    lower = 0; // lower limit of temperature table
-    upper = 300; // upper limit
-    step = 20; // step size
 
     printf("Using while loop ...\n");
-    fahr = lower;
-    while (fahr <= upper / 2) {
+    fahr = LOWER;
+    while (fahr <= UPPER / 2) {
         celsius = 5.0 * (fahr - 32.0) / 9.0;
         printf("%3.0f\t%6.1f\n", fahr, celsius);
-        fahr += step;
+        fahr += STEP;
     }
 
     printf("\n");
     printf("Using for loop ...\n");
-    for (; fahr <= upper; fahr += step) {
+    for (; fahr <= UPPER; fahr += STEP) {
         celsius = (5.0 / 9.0) * (fahr - 32);
         printf("%3.0f\t%6.1f\n", fahr, celsius);
     }
